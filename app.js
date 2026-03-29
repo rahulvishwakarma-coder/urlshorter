261880d74d49aaf6db52426ca8890d786efaa82b
rahulvishwakarma-coder <rahulvishwakarma88872@gmail.com>
2026-03-29 17:11:43 +0530

    version 4.0

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.json());
 
 import {urlRouter} from "./src/routes/url.routes.js";
 import {redirectUser} from "./src/controllers/shorten.controller.js"
+import connectDB from "./src/db/index.js";
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
