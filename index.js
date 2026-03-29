9172fe366b6f09f073a912a16c145183216a9e86
rahulvishwakarma-coder <rahulvishwakarma88872@gmail.com>
2026-03-29 17:10:14 +0530

    version 3.0

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import 'dotenv/config'
 import app from "./app.js"
 import connectDB from "./src/db/index.js"
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000;
 
 const main = async () => {
     await connectDB();
-    app.listen(PORT,()=>{
-        console.log(`server is running on PORT ${PORT}`)
-    })
-}
 
-main();
+    app.listen(PORT, () => {
+        console.log(`server is running on PORT ${PORT}`)
+    });
+};
+
+// only run locally
+if (process.env.NODE_ENV !== "production") {
+    main();
+}
