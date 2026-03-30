26a8ddf6bc03145f47f363f51f0c61da9ab87912
rahulvishwakarma-coder <rahulvishwakarma88872@gmail.com>
2026-03-30 19:33:24 +0530

    fix db connection

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(express.json());
 
 import {urlRouter} from "./src/routes/url.routes.js";
 import {redirectUser} from "./src/controllers/shorten.controller.js"
+import connectDB from "./src/db/index.js";
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.redirect("https://github.com/rahulvishwakarma-coder/urlshorter");
